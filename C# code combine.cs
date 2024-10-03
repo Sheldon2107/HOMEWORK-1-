0a08d9787aef91a56ee6e7f1df2e5a09d1b48ee2
Sheldon2107 <122601242+Sheldon2107@users.noreply.github.com>
2024-10-03 16:08:32 +0800

    Update and rename C# code (combine) to C# code combine.cs

--- C# code combine.cs
+++ C# code combine.cs
@@ -1,12 +1,7 @@
-// Author: Sheldon Stephen and Kampung Innovators Team
-// Team: Kampung Innovators
-// Project: Project Management System for Team and Task Management
-// Date: October 5, 2024
-
 using System;
 using System.Collections.Generic;
 
-// TeamMember Class
+
 public class TeamMember
 {
     public string Name { get; set; }
@@ -31,7 +26,7 @@ public class TeamMember
     }
 }
 
-// Task Class
+
 public class Task
 {
     public string TaskName { get; set; }
@@ -61,7 +56,6 @@ public class Task
     }
 }
 
-// Project Class
 public class Project
 {
     public string ProjectName { get; set; }
@@ -99,7 +93,7 @@ public class Project
     }
 }
 
-// Team Class
+
 public class Team
 {
     public string TeamName { get; set; }
@@ -131,7 +125,7 @@ public class Team
     }
 }
 
-// ProjectList Class
+
 public class ProjectList
 {
     public List<Project> ListOfProjects { get; set; }
@@ -162,30 +156,30 @@ public class ProjectList
     }
 }
 
-// Example Usage
+
 public class Program
 {
     public static void Main()
     {
-        // Creating Team Members
+        
         TeamMember member1 = new TeamMember("Nurul Aishah Elyani Binti Azhar", "Team Leader", "aishah@example.com");
         TeamMember member2 = new TeamMember("Nurul Fatihah Binti Rizwan", "Developer", "fatihah@example.com");
         TeamMember member3 = new TeamMember("Nadzira Binti Ramli", "Tester", "nadzira@example.com");
         TeamMember member4 = new TeamMember("Sheldon Stephen", "Developer", "sheldon@example.com");
         TeamMember member5 = new TeamMember("Nur Najla Alisa Binti Mat Suhairi", "Documentation Specialist", "najla@example.com");
 
-        // Creating a Project
+        
         Project project = new Project("Team Management System", DateTime.Now, DateTime.Now.AddMonths(2));
 
-        // Creating Tasks
+
         Task task1 = new Task("Design System", "Design the architecture of the system", DateTime.Now.AddDays(10));
         Task task2 = new Task("Develop Backend", "Develop the backend using C#", DateTime.Now.AddDays(30));
 
-        // Adding tasks to the project
+        
         project.AddTask(task1);
         project.AddTask(task2);
 
-        // Creating Team and assigning project
+       
         Team team = new Team("Kampung Innovators");
         team.AddMember(member1);
         team.AddMember(member2);
@@ -194,7 +188,6 @@ public class Program
         team.AddMember(member5);
         team.AssignProject(project);
 
-        // Showing Project Summary
         project.GetProjectSummary();
     }
 }
